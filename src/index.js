385df9cc4ed828e90f907675176d9caff918b132
Tien Le <tien06051992@gmail.com>
2016-12-08 11:30:25 +0700

    finish next pre nav

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-require('../sass/style.scss');
-const slider = document.getElementById('slides-one');
-const slides = slider.childElementCount;
-
-
-//console.log(slides);
-// var currSlide = 0;
-
-// const slideInterval = () => {
-//     setInterval(nextslide, 3000);
-// }
-
-// const nextslide = () => {
-//     slides[currSlide].className = 'slide';
-//     currSlide = (currSlide + 1) % slides.length;
-//     //console.log(currSlide + " - " + slides.length);
-//     slides[currSlide].className = 'slide showing';
-// }
-
 class Slider {
 	constructor(id) { // class constructor
-        this.slider = document.getElementById(id);
-        this.elementCount = slider.childElementCount;
+        self = this;
+        this.id = id;
+        this.slider = document.getElementById(this.id);
+        this.sliderBar = this.slider.children[0];
+        this.elementCount = this.sliderBar.childElementCount;
+        this.generateNav();
+        this.setupSliderBar();
+        this.currSlide = 0;
+        this.registerEventNavigation();
     }
 
     getId() {
@@ -33,21 +21,46 @@ class Slider {
 
     generateNav() {
     	const html = `<div class='nav-slider'>
-    			<span class='prev'>Prev</span>
-    			<span class='next'>Next</span>
-    		</div>
+    			         <span id="${this.id}-prev" class='prev'>Prev</span>
+    			         <span id="${this.id}-next" class='next'>Next</span>
+    		          </div>
     	`;
-        var div = document.createElement('div');
-        this.slider.parentElement.appendChild(div)
+        let div = document.createElement('div');
+        this.slider.appendChild(div)
         div.innerHTML = html;
-     //    var elements = div.childNodes;
-    	// this.slider.parentElement.appendChild(navSlider);
     }
 
+    setupSliderBar() {
+        for(let i = 0; i < this.elementCount; i++) {
+            this.sliderBar.children[i].style.width = this.slider.offsetWidth + 'px';
+        }
+        this.sliderBar.style.width = this.slider.offsetWidth * this.elementCount + 'px';
+    }
 
+    nextSlide() {
+        if(self.currSlide != self.elementCount) {
+            self.currSlide = (self.currSlide + 1) % self.elementCount;
+        }
+        console.log(self.currSlide);
+        self.animateSliderBar(self.currSlide);
+    }
+
+    prevSlide() {
+        if(self.currSlide != 0) {
+            self.currSlide = (self.currSlide - 1) % self.elementCount;
+        }
+        console.log(self.currSlide);
+        self.animateSliderBar(self.currSlide);
+    }
+
+    registerEventNavigation() {
+        document.getElementById(`${this.id}-prev`).addEventListener("click", this.prevSlide);
+        document.getElementById(`${this.id}-next`).addEventListener("click", this.nextSlide);
+    }
+
+    animateSliderBar(slideItem) {
+        self.sliderBar.style.transform = "translateX(" + -(slideItem * self.slider.offsetWidth) + "px)";
+    }
 
 }
-
 const sliderOne = new Slider('slide-one');
-sliderOne.generateNav();
-console.log(sliderOne.getSlideNumber());
