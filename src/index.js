3b3c948a4db99b4cdeda56a189eb0b84607b1be3
Tien Le Manh <tien.le@PYCO.VN>
2016-12-08 16:09:15 +0700

    style nav and dot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+"use strict";
 class Slider {
-	constructor(id) { // class constructor
+    constructor(id) { // class constructor
         self = this;
         this.id = id;
         this.slider = document.getElementById(this.id);
@@ -13,45 +14,48 @@ class Slider {
     }
 
     getId() {
-     	return this.id;
+        return this.id;
     }
 
     getSlideNumber() {
-    	return this.elementCount;
+        return this.elementCount;
     }
 
     generateNav() {
-    	const html = `<div class='nav-slider'>
-    			         <span id="${this.id}-prev" class='prev'>Prev</span>
-    			         <span id="${this.id}-next" class='next'>Next</span>
-    		          </div>
-    	`;
+        const html = `<div class='nav-slider'>
+                         <span id="${this.id}-prev" class='prev'></span>
+                         <span id="${this.id}-next" class='next'></span>
+                      </div>
+        `;
         let div = document.createElement('div');
-        this.slider.appendChild(div)
+        this.slider.appendChild(div);
         div.innerHTML = html;
     }
 
     generateDot() {
         let div = document.createElement('div');
-        div.className = 'dot-sliders'
-        for(let i = 0; i < this.elementCount; i++) {
+        div.className = 'dot-sliders';
+        for (let i = 0; i < this.elementCount; i++) {
             let item = document.createElement('span');
             item.setAttribute("data-item", i);
-            item.innerHTML = i + 1;
             div.appendChild(item);
         }
         this.slider.appendChild(div);
+
     }
 
     setupSliderBar() {
-        for(let i = 0; i < this.elementCount; i++) {
+        for (let i = 0; i < this.elementCount; i++) {
             this.sliderBar.children[i].style.width = this.slider.offsetWidth + 'px';
         }
         this.sliderBar.style.width = this.slider.offsetWidth * this.elementCount + 'px';
+        setTimeout(function() {
+            self.sliderBar.className += " transition";
+        },100);
     }
 
     nextSlide() {
-        if(self.currSlide != self.elementCount) {
+        if (self.currSlide != self.elementCount) {
             self.currSlide = (self.currSlide + 1) % self.elementCount;
         }
         console.log(self.currSlide);
@@ -59,9 +63,9 @@ class Slider {
     }
 
     prevSlide() {
-        if(self.currSlide != 0) {
+        if (self.currSlide != 0) {
             self.currSlide = (self.currSlide - 1) % self.elementCount;
-        }else {
+        } else {
             self.currSlide = self.elementCount - 1;
         }
         console.log(self.currSlide);
@@ -71,11 +75,17 @@ class Slider {
     registerEventNavigation() {
         document.getElementById(`${this.id}-prev`).addEventListener("click", this.prevSlide);
         document.getElementById(`${this.id}-next`).addEventListener("click", this.nextSlide);
+        //this.sliderBar.className += " transition";
     }
 
     animateSliderBar(slideItem) {
         self.sliderBar.style.transform = "translateX(" + -(slideItem * self.slider.offsetWidth) + "px)";
     }
 
+    slideTo(targetSlide) {
+        self.currSlide = targetSlide % self.elementCount;
+        console.log(self.currSlide);
+        self.animateSliderBar(self.currSlide);
+    }
 }
 const sliderOne = new Slider('slide-one');
